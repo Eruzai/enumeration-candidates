402c3b74b51b5b801e4776e4a96b9fcdd3d29670
Kevin Levasseur <kevin_levasseur@telus.net>
2024-05-22 11:27:26 -0700

    feat: return subset of candidates meeting certain criteria

--- filters.rb
+++ filters.rb
@@ -4,9 +4,10 @@ require './candidates'
 
 def find(id)
   @candidates.each { |candidate|
-  if candidate[:id] == id
-    return candidate
-  end }
+    if candidate[:id] == id
+      return candidate
+    end
+  }
   nil
 end
 
@@ -19,7 +20,44 @@ def experienced?(candidate)
 end
 
 def qualified_candidates(candidates)
-  # Your code Here
+  qualified = []
+  candidates.each { |candidate|
+    if experienced?(candidate) && hundred_github_points(candidate) && know_ruby_or_python(candidate) && applied_recently(candidate) && legal_age(candidate)
+      qualified << candidate
+    end
+  }
+  qualified
 end
 
 # More methods will go below
+def hundred_github_points(candidate)
+  if candidate[:github_points] >= 100
+    true
+  else
+    false
+  end
+end
+
+def know_ruby_or_python(candidate)
+  if candidate[:languages].include?("Ruby") || candidate[:languages].include?("Python")
+    true
+  else
+    false
+  end
+end
+
+def applied_recently(candidate)
+  if candidate[:date_applied] >= 15.days.ago.to_date
+    true
+  else
+    false
+  end
+end
+
+def legal_age(candidate)
+  if candidate[:age] >= 18
+    true
+  else
+    false
+  end
+end
